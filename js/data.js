b509a8603a221c04e3b6049eb033537f6ad7ddc9
Mattia Sinisi <mattia.sinisi@virgilio.it>
2022-02-06 16:48:31 +0100

    New main page!

--- js/data.js
+++ js/data.js
@@ -1,37 +1,40 @@
+const STATIC_RESOURCES_BASE_URL = "https://static.experimentalplayers.org/";
+const ICONS_BASE_URL = STATIC_RESOURCES_BASE_URL + "/icons";
+
 const sections = [
 	{
 		title: 'Spotify',
-		description: 'loremipsum loremipsum loremipsum loremipsum loremipsum loremipsum',
+		description: 'Una fantastica playlist che raccoglie tutti i gusti musicali degli <b>Experimental Players</b>!',
 		link: '#',
-		icon: '-',
+		icon: ICONS_BASE_URL + '/Spotify.png',
 		type: "is-success"
 	},
 	{
 		title: 'Hub',
-		description: 'loremipsum loremipsum loremipsum loremipsum loremipsum loremipsum',
-		link: '#',
-		icon: '-',
-		type: "is-info"
+		description: '<i>Coming Soon!</i>',
+		link: 'https://hub.experimentalplayers.org',
+		icon: ICONS_BASE_URL + '/Hub.png',
+		type: "is-warning"
 	},
 	{
 		title: 'Shop',
-		description: 'loremipsum loremipsum loremipsum loremipsum loremipsum loremipsum',
-		link: '#',
-		icon: '-',
+		description: 'Vestiti alla moda con il drip ufficiale 🥵 di EXP.',
+		link: 'https://shop.experimentalplayers.org',
+		icon: ICONS_BASE_URL + '/Shop.png',
 		type: "is-light"
 	},
 	{
 		title: 'Instagram',
-		description: 'loremipsum loremipsum loremipsum loremipsum loremipsum loremipsum',
-		link: '#',
-		icon: '-',
+		description: 'Seguici per non perderti mai un post.',
+		link: 'https://instagram.com/experimentalplayers',
+		icon: ICONS_BASE_URL + '/Instagram.png',
 		type: "is-danger"
 	},
 	{
 		title: 'GitHub',
-		description: 'loremipsum loremipsum loremipsum loremipsum loremipsum loremipsum',
-		link: '#',
-		icon: '-',
+		description: 'I nostri progetti, sempre e solo <i>open-source</i>.',
+		link: 'https://github.com/experimental-players/',
+		icon: ICONS_BASE_URL + '/GitHub.png',
 		type: "is-dark"
 	},
 ];

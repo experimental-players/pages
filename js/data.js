81b38e9d127c1fa91ec723dc8f16ab1aa31e13d6
Mattia Sinisi <mattia.sinisi@virgilio.it>
2022-02-06 11:56:26 +0100

    Created section logic

---
+++ js/data.js
@@ -0,0 +1,37 @@
+const sections = [
+	{
+		title: 'Spotify',
+		description: 'loremipsum loremipsum loremipsum loremipsum loremipsum loremipsum',
+		link: '#',
+		icon: '-',
+		type: "is-success"
+	},
+	{
+		title: 'Hub',
+		description: 'loremipsum loremipsum loremipsum loremipsum loremipsum loremipsum',
+		link: '#',
+		icon: '-',
+		type: "is-info"
+	},
+	{
+		title: 'Shop',
+		description: 'loremipsum loremipsum loremipsum loremipsum loremipsum loremipsum',
+		link: '#',
+		icon: '-',
+		type: "is-light"
+	},
+	{
+		title: 'Instagram',
+		description: 'loremipsum loremipsum loremipsum loremipsum loremipsum loremipsum',
+		link: '#',
+		icon: '-',
+		type: "is-danger"
+	},
+	{
+		title: 'GitHub',
+		description: 'loremipsum loremipsum loremipsum loremipsum loremipsum loremipsum',
+		link: '#',
+		icon: '-',
+		type: "is-dark"
+	},
+];
